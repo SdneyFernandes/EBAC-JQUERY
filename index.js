97991113e18dd1a3c71393378f8e077885bf7a8a
SdneyFernandes <fsidney987@gmail.com>
2023-12-19 03:25:24 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $('#tarefaForm').submit(function(event) {
+        event.preventDefault();
+
+        var taskName = $('#tarefaName').val();
+
+        $('#tarefaList').append('<li>' + taskName + '</li>');
+
+        $('#tarefaName').val('');
+    });
+
+    $('#tarefaList').on('click', 'li', function() {
+        $(this).toggleClass('completed');
+    });
+});
